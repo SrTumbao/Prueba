a5ab3cf546768974fa7666ad4ddc86ce515e87e6
SrTumbao <irodriguezs1900@alumno.ipn.mx>
2022-05-16 10:24:44 -0500

    espero que esta chingadera funcione

---
+++ script.js
@@ -0,0 +1,16 @@
+const $canvas = document.querySelector('canvas'),
+$btnDescargar = document.querySelector('btnDescargar');
+
+
+
+// Listener del botón
+$btnDescargar.addEventListener("click", () => {
+      // Crear un elemento <a>
+      let enlace = document.createElement('a');
+      // El título
+      enlace.download = "Canvas como imagen.png";
+      // Convertir la imagen a Base64 y ponerlo en el enlace
+      enlace.href = canvas.toDataURL();
+      // Hacer click en él
+      enlace.click();
+});
